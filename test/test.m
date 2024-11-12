ce5687e2958c99db4293a92c63273e3971265f39
stardusk2nd <unite0018@gmail.com>
2024-11-12 09:54:53 +0900

    Update test.m

--- test/test.m
+++ test/test.m
@@ -3,12 +3,17 @@ x = [+0+7000i, +1000+6000i, +2000+5000i, +3000+4000i,...
     +4000+3000i, +5000+2000i, +6000+1000i, +7000+0i];
 
 % output signal X
-% X_tmp = [0. +3500+3500i, 4. -500+499i, 2. +0+999i, 6. -1000-1i,
-%    1. +707+1706i, 5. -707+292i, 3. -293+707i, 7. -1707-708i];
+X_reversed = [+3500+3500i, -500+499i, +0+999i, -1000-1i,...
+              +707+1706i, -707+292i, -293+707i, -1707-708i] * 8;
 
-% 비트 재반전 및 truncation 보정
-X = [+3500+3500i, +707+1706i, +0+999i, -293+707i,...
-    -500+499i, -707+292i, -1000-1i, -1707-708i] *8;
+N = 8;
+num_bits = log2(N);
+X = zeros(1,N);
+
+for i = 0:N-1
+    reversed_idx = bit_reverse(i,num_bits);
+    X(reversed_idx + 1) = X_reversed(i + 1);
+end
 
 % inverse fft
 x_2 = ifft(X,8);
@@ -27,3 +32,9 @@ xlabel('Real Part');
 ylabel('Imaginary Part');
 legend;
 grid on;
+
+function reversed_index = bit_reverse(index, num_bits)
+    bin_str = dec2bin(index, num_bits);
+    reversed_bin_str = flip(bin_str);
+    reversed_index = bin2dec(reversed_bin_str);
+end
